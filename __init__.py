0c22b7163006c3ef22a5b054e14ca0be3190f5d1
Hansel Dunlop <hansel@interpretthis.org>
2013-01-17 00:32:01 +0000

    Adds helper functions for setting tz info on datetimes and formatting cents to locale specific output`

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
-# Copyright 2012 Hansel Dunlop
+# Copyright 2013 Hansel Dunlop
 # All rights reserved
 #
 # Author: Hansel Dunlop - hansel@interpretthis.org
 #
 
+from datetime import datetime
+import locale
+
 from django.core.mail import EmailMultiAlternatives
 from django.template import Context
 from django.template.loader import get_template
+from django.utils.timezone import utc
 
+USER_LOCALE = 'en_US.utf8'
 
 def send_html_email(subject, template_stub, sender, recipients, context):
     email_txt = get_template(template_stub + '.txt').render(Context(context))
@@ -16,3 +21,19 @@ def send_html_email(subject, template_stub, sender, recipients, context):
     msg.attach_alternative(email_html, "text/html")
     msg.send()
 
+def wise_datetime_now():
+    return datetime.utcnow().replace(tzinfo=utc)
+
+def wise_datetime_from_date(dt):
+    now = wise_datetime_now()
+    return wise_datetime(
+        day=dt.day, month=dt.month, year=dt.year,
+        hour=now.hour, minute=now.minute, second=now.second,
+    )
+
+def wise_datetime(**kwargs):
+    return datetime(**kwargs).replace(tzinfo=utc)
+
+def format_cents(cents):
+    locale.setlocale(locale.LC_ALL, USER_LOCALE)
+    return locale.currency(cents / 100.0, symbol=False, grouping=True)

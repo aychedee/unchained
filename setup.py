85524385bed73c69c483e8ea6bf7168183439481
Hansel Dunlop <hansel@interpretthis.org>
2013-01-17 08:24:02 +0000

    Adds minimum viable setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='Unchained',
+    version='1.0',
+    description='Some helper functions for sharing between Django projects',
+    author='Hansel Dunlop',
+    author_email='hansel@interpretthis.org',
+    url='https://github.com/aychedee/unchained',
+    packages=['unchained'],
+    requires=[
+        'django',
+    ]
+
+)
